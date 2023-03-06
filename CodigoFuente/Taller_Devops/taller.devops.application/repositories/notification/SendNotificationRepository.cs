76490930dea1d8ed528ccf4a03e1fcb45f27116d
Pedro Abel <parivera@angeluzcorp.com>
2023-03-06 00:05:12 -0500

    se carga repositorio

---
+++ CodigoFuente/Taller_Devops/taller.devops.application/repositories/notification/SendNotificationRepository.cs
@@ -0,0 +1,26 @@
+﻿using taller.devops.application.interfaces.services;
+using taller.devops.application.models.dtos.notifications;
+
+namespace taller.devops.application.repositories.notification
+{
+    public class SendNotificationRepository : ISendNotification
+    {
+        public SendNotificationRepository()
+        {
+
+        }
+
+        public (bool TrxExistosa, object Data) SendTransaccion(DTOSendNotification sendNotification)
+        {
+            try
+            {
+
+            }
+            catch (Exception EX)
+            {
+                throw new Exception("Error al enviar la trasacción.");
+
+            }
+        }
+    }
+}
--- CodigoFuente/Taller_Devops/taller.devops.domain/Repositories/SendNotificationRepository.cs
+++ CodigoFuente/Taller_Devops/taller.devops.domain/Repositories/SendNotificationRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace taller.devops.domain.Repositories
+﻿namespace taller.devops.domain.Repositories
 {
     public class SendNotificationRepository
     {
