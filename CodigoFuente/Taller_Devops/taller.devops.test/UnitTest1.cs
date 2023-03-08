47331998820e39586c2f3b2067e27d1bff011f70
gitName <a@b.c>
2023-03-07 21:11:24 -0500

    carga de prueba unitaria

--- CodigoFuente/Taller_Devops/taller.devops.test/UnitTest1.cs
+++ CodigoFuente/Taller_Devops/taller.devops.test/UnitTest1.cs
@@ -3,9 +3,20 @@ namespace taller.devops.test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void SendNotification()
         {
+            SendNotificationRepository SendTransaccion = new();
 
+
+            var result = SendTransaccion.SendTransaccion(new application.models.dtos.notifications.DTOSendNotification() 
+            {
+                From = "Rita Asturia",
+                Message = "This is a test",
+                TimeToLifeSec = 45,
+                To = "Juan Perez"
+            });
+
+            Assert.Equal("Hello Rita Asturia your message will be send", result.ToString());
         }
     }
 }
